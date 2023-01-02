27fb8a3cea773e4c803ef19e055cea746089bb6f
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-01-02 19:37:43 +0100

    Get the request

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,36 @@
 'use strict'
 
+/*-----------------------------------------------------------------*/
+
 import 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js';
 import 'https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js';
 
+/*-----------------------Import-functions.js-----------------------*/
+
+import  * as library  from './moduli/functions.js';
+
+
+let baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+let newStories = 'newstories.json';
+const NEWS_LIMIT = 10;
+
+getRequest( baseUrl + newStories );
+
+/*-------------------Internal-Function-Declaration-----------------*/
+
+function getRequest(url){
+
+    axios.get( url, {
+        params:{
+            print: 'pretty',
+        }
+    })
+    .then((response) => {
+        console.log(response);
+    }
+        
+    )
+    .catch( (err) => {
+        library.forErrors(err);
+    } );
+}
