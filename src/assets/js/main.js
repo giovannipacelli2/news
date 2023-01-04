5449a09b8b36b1ca0e2dcd2012e8a867373e3367
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-01-04 12:30:21 +0100

    Added new Classes that exstends Notice

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,16 +8,21 @@ import 'https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js';
 /*-----------------------Import-functions.js-----------------------*/
 
 import  * as library  from './moduli/functions.js';
-import Notice from './moduli/notice.js';
+import { Notice, Story, Comment } from './moduli/notice.js';
 
 
 let baseUrl = 'https://hacker-news.firebaseio.com/v0/';
 let newStories = 'newstories.json';
+
+let newStoriesId = null;
+
 const NEWS_LIMIT = 10;
 
 let seeNews = 0;
 
 getRequest( baseUrl + newStories );
+
+/* printElement('2921983'); */
 
 /*------------------------------------Internal-Function-Declaration-----------------------------------*/
 
@@ -31,9 +36,9 @@ function getRequest(url){
         }
     })
     .then((response) => {
-        let news = _.slice(response.data, seeNews, NEWS_LIMIT);
-        seeNews = news.length;
-        getNoticeById(news);
+        newStoriesId = response.data;
+        printElement(newStoriesId[102]); /* Call print one element */
+        getNoticeById(seeMore(response));
     })
     .catch( (err) => { library.forErrors(err) } );
 }
@@ -67,11 +72,47 @@ function getNoticeById(news) {
 function writeNotice(news){
 
     for ( let n of news ) {
-        let notice = new Notice(n.title, n.text, n.time, n.url);
+
+        let notice = null;
+
+        if ( n.type == "story" ){
+            notice = new Story(n.by, n.id, n.time, n.type, n.title, n.url, n.score);
+        }
+        if ( n.type == "comment" ){
+            notice = new Comment(n.by, n.id, n.time, n.type, n.parent, n.text);
+        }
+
         let card = notice.createCard();
 
         let container = library.getPageElement(".cards-container");
 
         container.insertAdjacentHTML('beforeend',card);
     }
+}
+
+/*------------------------Get-id-of-more-news----------------------*/
+
+function seeMore(response) {
+    let news = _.slice(response.data, seeNews, NEWS_LIMIT);
+    seeNews = news.length;
+
+    return news;
+}
+
+/*--------------------------Get-one-element------------------------*/
+
+function printElement(elem) {
+    debugger;
+    let url = baseUrl + 'item/' + elem + '.json';
+
+    axios.get( url, {
+        params:{
+            print: 'pretty',
+        }
+    })
+    .then((response) => {
+        debugger;
+        writeNotice([response.data]);
+    })
+    .catch( (err) => { library.forErrors(err) } );
 }
--- src/assets/js/moduli/notice.js
+++ src/assets/js/moduli/notice.js
@@ -1,11 +1,33 @@
-export default class Notice{
+export class Notice{
 
-    constructor(title = 'Titolo', text = "", date, url) {
-        this.title = title;
-        this.text = text;
-        this.date = new Date(date*1000);
-        this.fullDate = this.date.getDate() + "/" + (this.date.getMonth()+1) + "/" + this.date.getFullYear();
-        this.url = url;
+    constructor(by, id, time, type) {
+        this.by = by;
+        this.id = id;
+        this.time = time;
+        this.fullDate = this.convertTime(this.time);
+        this.type = type;
+    }
+
+    convertTime(time) {
+        let date = new Date( time * 1000 );
+
+        let day = date.getDate(); 
+        let mounth = date.getMonth() + 1; 
+        let year = date.getFullYear();
+
+        let fullItData;  
+
+        if ( day > 0 && day < 10 ) {
+            day = "0" + day;
+        }
+
+        if ( mounth > 0 && mounth < 10 ) {
+            mounth = "0" + mounth;
+        }
+
+        fullItData = day + "/" + mounth + "/" + year;
+
+        return fullItData;
     }
 
     createCard(){
@@ -13,8 +35,9 @@ export default class Notice{
             <div class="card cards">
                 <div class="card-body">
 
-                <h5 class="card-title">${this.title}</h5>
-                <p class="card-text">${this.text}</p>
+                <h5 class="card-title">${_.capitalize(this.type)}</h5>
+                <p class="card-text by">By: ${this.by}</p>
+                <p class="card-text">Vuoto</p>
                 <p class="card-text">${this.fullDate}</p>
                 <a href="${this.url}" target="_blank" class="card-link">News link</a>
 
@@ -24,3 +47,63 @@ export default class Notice{
         return body;
     }
 };
+
+export class Story extends Notice{
+    constructor(by, id, time, type, title, url, score){
+        super(by, id, time, type);
+        this.title = title;
+        this.url = url;
+        this.score = score;
+    }
+
+    createCard() {
+        let body = `
+            <div class="card cards">
+                <div class="card-body px-4">
+
+                    <h5 class="card-title">${this.title}</h5>
+                    <p class="card-text by">By: ${this.by}</p>
+                    <p class="card-text text-end me-5">Data:</p>
+                    <p class="card-text text-end me-4">${this.fullDate}</p>
+                    <a href="${this.url}" target="_blank" class="card-link">News link</a>
+
+                </div>
+            </div>
+        `;
+        return body;
+    }
+}
+
+export class Comment extends Notice{
+    constructor(by, id, time, type, parent, text = ""){
+        super(by, id, time, type);
+        this.parent = parent;
+        this.text = text;
+    }
+
+    createCard() {
+        let body = `
+            <div class="card cards">
+                <div class="card-body px-4">
+
+                    <h5 class="card-title">${_.capitalize(this.type)}</h5>
+                    <p class="card-text by">By: ${this.by}</p>
+                    <p class="card-text">${this.text}</p>
+                    <p class="card-text text-end me-5">Data:</p>
+                    <p class="card-text text-end me-4">${this.fullDate}</p>
+
+                </div>
+            </div>
+        `;
+        return body;
+    }
+}
+
+export class Ask extends Notice{
+    constructor(by, id, time, type, text, title, score){
+        super(by, id, time, type);
+        this.text = text;
+        this.title = title;
+        this.score = score;
+    }
+}
